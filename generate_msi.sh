163192cec747e3ac20bc85cb72e96c9ef6c9fa82
juanma1980 <juanma1980@gmail.com>
2020-08-15 09:37:38 +0200

    The windows installer

---
+++ generate_msi.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+PROJECT="LliuWin"
+VERSION="19.07"
+DEVEL="LliureX Team"
+ARCH="x64"
+UPID="00112232-GNUX-LL00-1907-019283746547"
+DIST=./dist
+MSI=${DIST}/lliuwin_installer_x64.msi
+ICO=./data/images/lliuwin.ico
+BUILD=./build
+EXE=lliuwin.exe
+mkdir ${DIST} 2>/dev/null
+cp ${BUILD}/${EXE} ${DIST}
+msi-packager -n "$PROJECT" -v "$VERSION" -m "$DEVEL" -a "$ARCH" -u "$UPID" -i $ICO -e "$EXE" $DIST $MSI
+if [[ $? -eq 0 ]]
+then
+	echo "Generated $MSI"
+fi
+
+exit $?
