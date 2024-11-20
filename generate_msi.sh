247555bb9f84361eb1dde9b3084122e90b1dbfa7
lliurex <juanma1980@gmail.com>
2024-11-20 06:51:08 +0100

    Make project if needed

--- generate_msi.sh
+++ generate_msi.sh
@@ -9,14 +9,36 @@ MSI=${DIST}/lliuwin_installer_x64.msi
 ICO=./data/images/lliuwin.ico
 BUILD=./build
 EXE=lliuwin.exe
-echo "Generating msi"
-mkdir ${DIST} 2>/dev/null
-cp ${BUILD}/${EXE} ${DIST}
-echo msi-packager -n "$PROJECT" -v "$VERSION" -m "$DEVEL" -a "$ARCH" -u "$UPID" -i $ICO -e "$EXE" $DIST $MSI
-msi-packager -n "$PROJECT" -v "$VERSION" -m "$DEVEL" -a "$ARCH" -u "$UPID" -i $ICO -e "$EXE" $DIST $MSI
-if [[ $? -eq 0 ]]
-then
-	echo "Generated $MSI"
-fi
+MAKEOPTS=""
 
-exit $?
+function build
+{
+	make clean
+	make $MAKEOPTS
+}
+
+function msi_build
+{
+	echo "Generating msi"
+	echo "--------------"
+	cp ${BUILD}/${EXE} ${DIST}
+	echo msi-packager -n "$PROJECT" -v "$VERSION" -m "$DEVEL" -a "$ARCH" -u "$UPID" -i $ICO -e "$EXE" $DIST $MSI
+	msi-packager -n "$PROJECT" -v "$VERSION" -m "$DEVEL" -a "$ARCH" -u "$UPID" -i $ICO -e "$EXE" $DIST $MSI
+	RET=$?
+	if [[ $RET -eq 0 ]]
+	then
+		echo "--------------"
+		echo "Generated $MSI"
+	else
+		echo "##############"
+		echo "BUILD FAILED"
+		echo "##############"
+	fi
+}
+
+RET=1
+mkdir ${DIST} 2>/dev/null
+[ ! -e ${BUILD}/${EXE} ] && build
+msi_build
+
+exit $RET
